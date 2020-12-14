b8edeb6401a6230290297650266a4fe2ec7e0094
Alessandro Candido <candido.ale@gmail.com>
2020-12-14 15:52:37 +0100

    Finish exercise 14 part 1 year 2020

--- xmas2020/src/bin/ex14_1y20.rs
+++ xmas2020/src/bin/ex14_1y20.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
 use aocinput::request;
 
 #[derive(Clone, Copy, Debug)]
@@ -14,7 +18,7 @@ impl BitMask {
     fn from_str(s: &str) -> BitMask {
         let mut zeros = 0;
         let mut ones = 0;
-        for (i, c) in s.trim().chars().enumerate() {
+        for (i, c) in s.trim().chars().rev().enumerate() {
             match c {
                 '0' => {
                     zeros += 1 << i;
@@ -32,22 +36,36 @@ impl BitMask {
     }
 
     fn apply(&self, n: u64) -> u64 {
-        n
+        //println!("{}", self.ones);
+        (n | self.ones) & !self.zeros
     }
 }
 
 fn main() {
     let resp = request::get_input(2020, 14);
 
+    let mut memory = HashMap::new();
     let mut mask = BitMask::new();
 
     for line in resp.trim().split("\n") {
         if line.contains('[') {
-            //println!("{}", line);
+            let (loc, value): (u64, u64) = line
+                .trim()
+                .split(|c| ['[', ']', '='].contains(&c))
+                .filter(|s| ![" ", "mem"].contains(&s))
+                .map(|s| s.trim().parse().unwrap())
+                .collect_tuple()
+                .unwrap();
+            memory.insert(loc, mask.apply(value));
+        //println!("{}: {}", loc, mask.apply(value));
         } else {
-            //mask = line.split("=").collect()[1].parse();
             mask = BitMask::from_str(line.split("=").collect::<Vec<&str>>()[1]);
-            println!("{:?}", mask);
+            //println!("{:?}", mask);
         }
     }
+
+    println!("{}", memory.values().sum::<u64>());
+    //let value = 0;
+    //let mask = BitMask::from_str("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+    //println!("value: {}\nresult: {}", value, mask.apply(value));
 }
